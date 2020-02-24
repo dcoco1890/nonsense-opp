de7ea6dd8d7bb9f12c29b771ae9caa3438aa83da
Cohen <dcoco91@gmail.com>
2020-02-24 14:45:17 -0500

    changed initial props for pokelist page to only grab first 151 poke. Maybe I'll add some functionality to sort pokemon

--- pages/pokelist.js
+++ pages/pokelist.js
@@ -19,11 +19,16 @@ const pokelist = props => {
 pokelist.getInitialProps = async function() {
   const res = await fetch("https://unpkg.com/pokemons@1.1.0/pokemons.json");
   const data = await res.json();
-
+  // Added this to prevent the Mega Evolutions from showing twice. The picture
+  // is the same for both reg and mega so this made more sense.
+  let oldNum = 0;
+  let incomingNum = 1;
   return {
-    poke: data.results.filter((item, i) => {
-      if (i < 200) {
-        if (!item.evolution) {
+    poke: data.results.filter(item => {
+      if (oldNum !== 151) {
+        incomingNum = parseInt(item.national_number);
+        if (oldNum !== incomingNum) {
+          oldNum = incomingNum;
           return item;
         }
       }
